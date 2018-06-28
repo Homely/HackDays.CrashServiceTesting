63939f2c7f5dee9c767bbd3ed4b8dc9c88c30b41
Pure Krome <pure.krome@world-domination.com.au>
2018-06-28 12:18:09 +1000

    Starting template for Web App.

---
+++ src/WebApplication/Controllers/ErrorsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplication.Controllers
+{
+    [Route("")]
+    public class ErrorsController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Details()
+        {
+            return View();
+        }
+
+        // GET: api/<controller>
+        [HttpGet("error1")]
+        public ActionResult Error1()
+        {
+            throw new Exception();
+        }
+
+        [HttpGet("error2")]
+        public ActionResult Error2()
+        {
+            throw new Exception("Some error occured");
+        }
+
+        [HttpGet("error3")]
+        public ActionResult Error3()
+        {
+            var innerException = new Exception("i'm an inner exception");
+            throw new Exception("Some error occured", innerException);
+        }
+    }
+}
